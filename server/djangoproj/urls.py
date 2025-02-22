7f96f7368641a30257ebfa9f4b1a8c7bd4c1a9d1
karnesh <jain.karnesh@gmail.com>
2025-02-21 22:20:33 -0500

    added registration page

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
