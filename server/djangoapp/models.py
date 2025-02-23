73960e28e7db7dc3d226035d3d1aa66ec98b7d79
karnesh <jain.karnesh@gmail.com>
2025-02-23 01:28:40 -0500

    Fixed linting issues

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
+
 # from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
@@ -19,17 +20,15 @@ class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023, 
-                               validators=[
-                                   MaxValueValidator(2023),
-                                   MinValueValidator(2015)
-                               ])
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+    year = models.IntegerField(
+        default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+    )
     # Other fields as needed
 
     def __str__(self):
