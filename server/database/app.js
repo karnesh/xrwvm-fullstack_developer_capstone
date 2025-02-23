ef9fcfcbe376607546fb76cd1164e0d19f8ed185
karnesh <jain.karnesh@gmail.com>
2025-02-23 01:54:23 -0500

    Fixed linting issues

--- server/database/app.js
+++ server/database/app.js
@@ -1,11 +1,13 @@
+/*jshint esversion: 8 */
+
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -24,11 +26,10 @@ try {
   });
   Dealerships.deleteMany({}).then(()=>{
     Dealerships.insertMany(dealerships_data['dealerships']);
-  });
-  
+  }); 
 } catch (error) {
   res.status(500).json({ error: 'Error fetching documents' });
-}
+};
 
 
 // Express route to home
