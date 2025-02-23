125df3b8f9b1d0c3ed5822488501cae34188660c
karnesh <jain.karnesh@gmail.com>
2025-02-22 23:26:03 -0500

    fixed backend urls

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path to get_dealerships
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
     # path to get dealer details from review
